1a3b3a876d83cb3af85c1c737054c02609a17949
rakhazan <rakhazahnur26@gmail.com>
2022-07-06 14:33:11 +0700

    :lipstick: Change dashboard layout

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -6,10 +6,6 @@ class Home extends CI_Controller
     public function index()
     {
 		$data['page'] = "Dashboard";
-        $this->load->view('layouts/header', $data);
-        $this->load->view('layouts/navbar');
-        $this->load->view('layouts/sidebar');
         $this->load->view('dashboard', $data);
-        $this->load->view('layouts/footer');
     }
 }
